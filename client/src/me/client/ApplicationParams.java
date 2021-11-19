959bd4f391fa8b7c8068056f6fb0450d195f0e38
DragoFantasma <18010330+DragoFantasma@users.noreply.github.com>
2021-11-19 18:47:46 +0100

    Removed unused code

--- client/src/me/client/ApplicationParams.java
+++ client/src/me/client/ApplicationParams.java
@@ -2,15 +2,8 @@ package me.client;
 
 public class ApplicationParams {
 
-    public static final String S_PROGRAM_NAME = "ClientDownloader";
     public static final int S_ARGUMENTS_REVERSED = 1;
-    public static String S_JAR_NAME;
+    public static String S_JAR_NAME = "clientdownloader.jar";
     public static boolean S_APPLICATION_SILENT = false;
-
-    static {
-        StackTraceElement[] stack = Thread.currentThread().getStackTrace ();
-        StackTraceElement main = stack[stack.length - 1];
-        S_JAR_NAME = main.getClassName();
-    }
 
 }
